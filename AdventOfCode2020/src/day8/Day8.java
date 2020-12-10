25c413af0cb55e0c1fb357dafd0d2bc2a0663149
Kaleb Fowler <kaleb.fowler6@gmail.com>
2020-12-09 22:38:28 -0600

    Done with Day 8, part 2.
    Passing the dupe tracker was dumb because after part 1, it was all True

--- AdventOfCode2020/src/day8/Day8.java
+++ AdventOfCode2020/src/day8/Day8.java
@@ -11,9 +11,9 @@ public class Day8 {
 		
 		ArrayList<String> data = inputToArray();//read input to a format I can more easily go back and forth
 		
-		ArrayList<Boolean> dupe = createDupeTracker(data.size());// fill an array with false, which we will use to track if we've executed a line of code before
+		//ArrayList<Boolean> dupe = createDupeTracker(data.size());// fill an array with false, which we will use to track if we've executed a line of code before
 		
-		int part1= accAtDupe(data, dupe);
+		int part1= accAtDupe(data, createDupeTracker(data.size()));
 		
 		
 		System.out.println("Answer for part 1: "+part1);
@@ -21,7 +21,7 @@ public class Day8 {
 		
 		//for part 2.  make two new methods, doesProgramTerminate(data, dupe, indexToFlip) + executeCorrectedProgram(data, dupe, indexToFlip)
 		//maybe needs a third method testFlipAll(data, dupe), returning int indexToFlip.   If we parse it as it would be written, we don't test flips for any lines that would never be ran anyway.  
-		int part2Flip=testFlipAll(data,dupe);//get the index of the instruction to flip
+		int part2Flip=testFlipAll(data,createDupeTracker(data.size()));//get the index of the instruction to flip
 		if (part2Flip==-1)//abort
 			System.out.println("Did not find flippable command");
 		else {
@@ -79,8 +79,15 @@ public class Day8 {
 	public static int testFlipAll(ArrayList<String> data, ArrayList<Boolean> dupe) {
 		//int accumulator=0;
     	int index=0;
-		
+    	//int trackHowManyCommands=0;
+    	//System.out.println(trackHowManyCommands);
+    	
     	while (dupe.get(index)==false) {//heavily assuming there's an infinite loop here, oh well
+    		//System.out.println(trackHowManyCommands);
+			//trackHowManyCommands++;
+    		
+    		
+    		
     		//split into tokens, only separator is space
     		String delims = "[ ]";
 			String[] tokens=data.get(index).split(delims);
@@ -91,7 +98,7 @@ public class Day8 {
 			switch(tokens[0]) {
 			case "nop":
 				//then do nothing, increment index for next loop
-				if (doesProgramTerminate(data,dupe,index)) {
+				if (doesProgramTerminate(data,createDupeTracker(data.size()),index)) {
 					return index;
 					
 				}
@@ -104,7 +111,7 @@ public class Day8 {
 				break;
 			case "jmp":
 				//set index appropriately.  
-				if (doesProgramTerminate(data,dupe,index)) {
+				if (doesProgramTerminate(data,createDupeTracker(data.size()),index)) {
 					return index;
 					
 				}
@@ -123,9 +130,12 @@ public class Day8 {
 	
 	public static boolean doesProgramTerminate(ArrayList<String> data, ArrayList<Boolean> dupe, int indexToFlip) {
 		int index=0;
-		
+		//int trackHowManyCommands=0;
+		//System.out.println(trackHowManyCommands);
 		
 		while (dupe.get(index)==false) {//heavily assuming there's an infinite loop here, oh well
+			//System.out.println(trackHowManyCommands);
+			//trackHowManyCommands++;
     		//split into tokens, only separator is space
     		String delims = "[ ]";
 			String[] tokens=data.get(index).split(delims);
